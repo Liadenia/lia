38409a4ac374578beb85628727ddc668cffd6b70
Liadenia <121140535+Liadenia@users.noreply.github.com>
2022-12-21 11:27:54 +0900

    musicplayer_test

---
+++ js/listdata.js
@@ -0,0 +1,39 @@
+
+const musicList=[
+    {
+        name:"Alone",
+        artist:"Color Out",
+        img:"album1",
+        audio:"Alone"
+    },
+    {
+        name:"Mahidevran - Maze of sorrow",
+        artist:"Mahidevran rock band",
+        img:"album2",
+        audio:"Mahidevran - Maze of sorrow"
+    },
+    {
+        name:"No Rest Or Endless Res",
+        artist:"Lisofv",
+        img:"album3",
+        audio:"No Rest Or Endless Res"
+    },
+    {
+        name:"Old Melody",
+        artist:"TAB",
+        img:"album4",
+        audio:"Old Melody"
+    },
+    {
+        name:"Tantalizing Youth",
+        artist:"Social Square",
+        img:"album5",
+        audio:"Tantalizing Youth"
+    },
+    {
+        name:"The Deep",
+        artist:"Anitek",
+        img:"album6",
+        audio:"The Deep"
+    }
+]
