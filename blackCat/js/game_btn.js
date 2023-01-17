f58975033d74bb1b0e62ffffb9646fd4445450e6
Liadenia <121140535+Liadenia@users.noreply.github.com>
2023-01-17 12:27:14 +0900

    js update
    update

---
+++ blackCat/js/game_btn.js
@@ -0,0 +1,15 @@
+
+$(function(){
+    $(document).on('touchplay',function(){});
+    $('html,body').animate({scrollTop:'5px'}, 200);
+    $('.play').on('click',function(){
+        window.location.href="game_play.html";
+    });
+    $('.howtoplay').on('click',function(){
+        window.location.href="game_howtoplay.html";
+    });
+
+    $("h1, h2").on("click",function () {
+        window.location.href = "game_intro.html";
+    });
+});
