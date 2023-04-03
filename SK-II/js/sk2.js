11df78f91358b84f7f3f999e4d4b7edbc447f243
Liadenia <liden207@naver.com>
2023-04-03 23:15:44 +0900

    Update sk2.js

--- SK-II/js/sk2.js
+++ SK-II/js/sk2.js
@@ -1,15 +1,26 @@
-$(function() {
-   var nav = $('header#innerbox');
-   var navHeight = nav.outerHeight();
-   var section1 = $('section#banner');
+// $(function() {
+//    var nav = $('header#innerbox');
+//    var navHeight = nav.outerHeight();
+//    var section1 = $('section#banner');
    
-   $(window).scroll(function () {
-   var scrollTop = $(header).scrollTop();
+//    $(window).scroll(function () {
+//    var scrollTop = $(header).scrollTop();
 
-   if (scrollTop > section1.offset().top - navHeight) {
-      nav.addClass('scroll');
+//    if (scrollTop > section1.offset().top - navHeight) {
+//       nav.addClass('scroll');
+//    } else {
+//       nav.removeClass('scroll');
+//    }
+//    });
+// });
+
+window.addEventListener('scroll', function() {
+   var nav = document.getElementById('navigation');
+   var scrollPos = window.scrollY || window.scrollTop || document.getElementsByTagName("html")[0].scrollTop;
+   
+   if (scrollPos >= window.innerHeight) {
+   nav.classList.add('scrolled');
    } else {
-      nav.removeClass('scroll');
+   nav.classList.remove('scrolled');
    }
-   });
 });
