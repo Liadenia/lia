784d830ec7aeb824604abfe61890f88803a2c926
Liadenia <liden207@naver.com>
2023-04-05 01:34:00 +0900

    gnb finish

--- SK-II/js/sk2.js
+++ SK-II/js/sk2.js
@@ -1,4 +1,4 @@
-// -------------------------------------------------
+// ----------------aside tab 스크롤시 움직임---------------------------------
 $(function(){
    // const headerH=$('header').outerHeight();
    const defaultTop=parseInt($('aside').css('top'));
@@ -10,4 +10,54 @@ $(function(){
       }
       $('aside').stop().animate({top:targetPosition},800)
    });
+});
+
+// --------------------------------
+
+
+// $('.sub').hide();
+
+// $('.gnb>li').on('mouseenter', function () {
+// if ($(this).children('.sub').is(':hidden')) {
+//    $('.sub:visible').hide();
+//    $(this).children('.sub').stop().slideDown(200);
+// }
+// });
+
+// $('.gnb>li').on('mouseleave', function () {
+// $(this).children('.sub').stop().slideUp(200);
+// });
+
+// $('.gnb').on('mouseleave', function () {
+// $('.sub').stop().slideUp(200);
+// });
+
+let mouseoverstate = false;
+
+$('.sub').hide();
+$('.gnb>li>a>span').hide();
+
+$('.gnb>li').on('mouseenter', function () {
+  console.log('a mouseenter');
+  if (!mouseoverstate) {
+    $('.sub', this).stop().slideDown(200);
+    $('> a > span', this).show();
+  } else { // li간 이동진입
+    $('.sub:visible').hide(); //.stop().fadeIn(150);
+    $('.sub', this).show();
+    $('> a > span', this).show();
+  }
+  mouseoverstate = true;
+});
+
+$('.gnb>li').on('mouseleave', function () {
+  $('.sub', this).stop().slideUp(200);
+  $('> a > span').hide();
+  mouseoverstate = false;
+});
+
+$('.gnb').on('mouseleave', function () {
+  $('.sub').stop().slideUp(200)
+  $('.gnb>li>a>span').hide();
+  mouseoverstate = false;
 });
