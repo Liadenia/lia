4cf976f66ade6ba80b0b264ed640539e1106fb81
Liadenia <liden207@naver.com>
2023-04-05 17:25:38 +0900

    scroll color change

--- SK-II/js/sk2.js
+++ SK-II/js/sk2.js
@@ -12,25 +12,8 @@ $(function(){
    });
 });
 
-// --------------------------------
+// ---------------gnb mouseenter시 sub show -----------------
 
-
-// $('.sub').hide();
-
-// $('.gnb>li').on('mouseenter', function () {
-// if ($(this).children('.sub').is(':hidden')) {
-//    $('.sub:visible').hide();
-//    $(this).children('.sub').stop().slideDown(200);
-// }
-// });
-
-// $('.gnb>li').on('mouseleave', function () {
-// $(this).children('.sub').stop().slideUp(200);
-// });
-
-// $('.gnb').on('mouseleave', function () {
-// $('.sub').stop().slideUp(200);
-// });
 
 let mouseoverstate = false;
 
@@ -38,26 +21,45 @@ $('.sub').hide();
 $('.gnb>li>a>span').hide();
 
 $('.gnb>li').on('mouseenter', function () {
-  console.log('a mouseenter');
-  if (!mouseoverstate) {
-    $('.sub', this).stop().slideDown(200);
-    $('> a > span', this).show();
-  } else { // li간 이동진입
-    $('.sub:visible').hide(); //.stop().fadeIn(150);
-    $('.sub', this).show();
-    $('> a > span', this).show();
-  }
-  mouseoverstate = true;
+console.log('a mouseenter');
+if (!mouseoverstate) {
+   $('.sub', this).stop().slideDown(200);
+   $('> a > span', this).show();
+} else { // li간 이동진입
+   $('.sub:visible').hide(); //.stop().fadeIn(150);
+   $('.sub', this).show();
+   $('> a > span', this).show();
+}
+mouseoverstate = true;
 });
 
 $('.gnb>li').on('mouseleave', function () {
-  $('.sub', this).stop().slideUp(200);
-  $('> a > span').hide();
-  mouseoverstate = false;
+$('.sub', this).stop().slideUp(200);
+$('> a > span').hide();
+mouseoverstate = false;
 });
 
 $('.gnb').on('mouseleave', function () {
-  $('.sub').stop().slideUp(200)
-  $('.gnb>li>a>span').hide();
-  mouseoverstate = false;
+$('.sub').stop().slideUp(200)
+$('.gnb>li>a>span').hide();
+mouseoverstate = false;
+});
+
+// -----------------------------
+$(function(){
+   // 스크롤 시 header fade-inline
+   $(document).on('scroll', function(){
+      if($(window).scrollTop() > 100){
+         $("#header").removeClass("deactive");
+         $("#header").addClass("active");
+         $('.gnb').addClass('scrolled');
+         $('h1 img').attr('src','attr/logo/logo-pink.png');
+      }else{
+         $("#header").removeClass("active");
+         $("#header").addClass("deactive");
+         $('.gnb').removeClass('scrolled');
+         $('h1 img').attr('src','attr/logo/logo-white.png');
+      }
+   })
+   
 });
