4d31121c779c38f21ddde1ca9ec80ee7225c8dc9
Liadenia <liden207@naver.com>
2023-04-08 03:35:47 +0900

    css 수정 및 스크롤 페이지 완료

---
+++ iceCream/js/icecream.js
@@ -0,0 +1,20 @@
+
+
+let newsWrapper = document.querySelector('.news_wrapper');
+let controlBar = document.querySelector('.control_bar');
+let ctrBtn = document.querySelector('.ctr_btn');
+
+newsWrapper.addEventListener('scroll', function() {
+  let scrollPercentage = newsWrapper.scrollTop / (newsWrapper.scrollHeight - newsWrapper.clientHeight);
+  let controlBarHeight = newsWrapper.clientHeight * scrollPercentage;
+  let ctrBtnTop = newsWrapper.clientHeight * scrollPercentage - ctrBtn.clientHeight / 2;
+
+  controlBar.style.height = '680px';
+  controlBar.style.top = '0px';
+  controlBar.style.position = 'relative';
+
+  let maxBtnTop = 680 - ctrBtn.clientHeight;
+  let btnTop = Math.min(maxBtnTop, scrollPercentage * maxBtnTop);
+  ctrBtn.style.top = btnTop + 'px';
+  ctrBtn.style.position = 'relative';
+});
